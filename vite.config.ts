5507650e34814bab48aa3873ba27f1a70eaef0da
Lara Dias <lara.dias@estudante.ufjf.br>
2024-12-17 10:03:23 -0300

    ajustes github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ufjf-dcc207-2024-3-a-atv05-Lara-Dias",
+  base: "/ufjf-dcc207-2024-3-a-atv05-lara-dias/",
   plugins: [react()],
 })

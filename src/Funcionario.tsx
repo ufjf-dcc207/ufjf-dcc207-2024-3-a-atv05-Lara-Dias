30cebfe0a225f205df3e8fa106c60323920c8c2e
Lara Dias <lara.dias@estudante.ufjf.br>
2024-12-17 09:10:53 -0300

    calcula abono salarial

--- src/Funcionario.tsx
+++ src/Funcionario.tsx
@@ -5,7 +5,7 @@ import Dependentes from './Dependentes';
 interface FuncionarioProps{
     nome:string;
     setor:string;
-    salario:string;
+    salario:number;
     data: string;
 }
 
@@ -13,15 +13,21 @@ interface FuncionarioProps{
 function Funcionario( {
         nome = dados.funcionarios[0].nome_completo,
         setor = dados.funcionarios[0].setor,
+        salario = dados.funcionarios[0].salario_base,
         data = dados.funcionarios[0].data_admissao,
-        
     }: FuncionarioProps){
+        const dependentes = dados.funcionarios[0].dependentes;
+        const totalAbono = dependentes.reduce((sum,dep) => sum +dep.abono_salarial,0);
+        const salarioFinal = salario + totalAbono;
+
     return(
         <>
             <div className="funcionario"> 
-                <div>{nome}</div>
-                <div>{setor}</div>
-                <div>{data}</div>
+                <h2>{nome}</h2>
+                <p>{setor}</p>
+                <p>{new Date(data).toLocaleDateString()}</p>
+                <p>Salário Base: R$ {salario.toFixed(2)}</p>
+                <p>Salário Final (com abonos): R$ {salarioFinal.toFixed(2)}</p>
             </div>
 
             <Dependentes/>
